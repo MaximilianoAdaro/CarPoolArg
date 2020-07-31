7c1581f378a6fc84a3f3d877f8b726a1539ac7ab
Numa Leone <numaleoneelizalde@gmail.com>
2020-07-30 22:34:39 -0300

    mail

---
+++ src/main/java/austral/ing/lab1/service/sendEmail.java
@@ -0,0 +1,62 @@
+package austral.ing.lab1.service;
+
+import java.util.Properties;
+import javax.mail.Session;
+import javax.mail.Authenticator;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class sendEmail {
+    public static void sendMail(String recepient) throws Exception{
+
+        System.out.println("preparing to send mail");
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        String myAccount = "carpoolarg2020@gmail.com";//change accordingly
+        String password = "carpoolarg2020";//change accordingly
+
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccount, password);
+            }
+        });
+
+        Message message = prepareMessage(session, myAccount, recepient);
+
+        System.out.println("prepara mensaje 2");
+
+        Transport.send(message);
+        System.out.println("message sent successfully");
+
+    }
+    private static Message prepareMessage (Session session, String account, String recepient) {
+        System.out.println("mandando mensaje");
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(account));
+            message.setRecipient(Message.RecipientType.TO,new InternetAddress(recepient));
+            message.setSubject("Testing");
+            message.setText("Hola broder");
+            return message;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+
+
+
