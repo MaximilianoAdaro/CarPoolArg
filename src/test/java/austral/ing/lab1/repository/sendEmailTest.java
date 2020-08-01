65e55b1fbfd55ce0b537c01661dc98635d62e2be
Numa Leone <numaleoneelizalde@gmail.com>
2020-08-01 19:55:15 -0300

    mail

---
+++ src/test/java/austral/ing/lab1/repository/sendEmailTest.java
@@ -0,0 +1,13 @@
+package austral.ing.lab1.repository;
+
+
+import austral.ing.lab1.service.email.sendEmail;
+
+public class sendEmailTest {
+
+    public static void main(String[] args) throws Exception {
+
+        sendEmail.sendMail("numaleoneelizalde@gmail.com");
+    }
+
+}
