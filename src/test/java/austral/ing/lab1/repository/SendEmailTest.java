5aacd1c0f377de0a2762c3cb44e8a347264973b1
MaximilianoAdaro <maxiadaro1999@gmail.com>
2021-02-11 01:36:57 -0300

    some changes

--- src/test/java/austral/ing/lab1/repository/SendEmailTest.java
+++ src/test/java/austral/ing/lab1/repository/SendEmailTest.java
@@ -3,7 +3,7 @@ package austral.ing.lab1.repository;
 
 import austral.ing.lab1.service.email.SendEmailService;
 
-public class SendEmailTest {
+class SendEmailTest {
 
     public static void main(String[] args) throws Exception {
         String email = "maxiadaro1999@gmail.com";
